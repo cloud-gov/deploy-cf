27d8e381e203fac0d362daee8ed6af37eb5acaa3
James C. Scott <jamesc.scott@gsa.gov>
2015-12-14 12:44:33 -0500

    Update yamls to match v4 deploy of 18-cf

--- generate-staging.sh
+++ generate-staging.sh
@@ -13,3 +13,4 @@ spiff merge \
 sed -i -- 's/10.10/10.9/g' manifest-staging.yml
 # Reverts this IP back for the cg-metrics
 sed -i -- 's/10.9.101.63/10.10.101.63/g' manifest-staging.yml
+sed -i -- 's/10.9.1.7\n/10.10.1.7\n/g' manifest-staging.yml
