da28159372fe3fad91e08409c33d52b841b1977a
timothy-spencer <timothy.spencer@gsa.gov>
2018-05-08 11:00:10 -0700

    changed script to be able to take arbitrary inputs, changed name to be less sandboxy

--- ci/update-isolation-segments.sh
+++ ci/update-isolation-segments.sh
@@ -11,8 +11,15 @@ cf create-isolation-segment "${PLATFORM_ISOLATION_SEGMENT}"
 cf enable-org-isolation "${PLATFORM_ORGANIZATION}" "${PLATFORM_ISOLATION_SEGMENT}"
 cf set-space-isolation-segment "${PLATFORM_SPACE}" "${PLATFORM_ISOLATION_SEGMENT}"
 
-# Create isolation segment for sandbox applications that want to use volume services
-cf target -o "${SANDBOX_ORGANIZATION}"
-cf create-isolation-segment "${SANDBOX_ISOLATION_SEGMENT}"
-cf enable-org-isolation "${SANDBOX_ORGANIZATION}" "${SANDBOX_ISOLATION_SEGMENT}"
-cf set-space-isolation-segment "${SANDBOX_SPACE}" "${SANDBOX_ISOLATION_SEGMENT}"
+# Create isolation segment for volume-enabled applications that want to use volume services
+# VOLUME_TARGETS should be something like "org:space1,space2 org1:space3"
+cf create-isolation-segment "${VOLUME_ISOLATION_SEGMENT}"
+for i in ${VOLUME_TARGETS} ; do
+  org="$(echo $i | awk -F: '{print $1}')"
+  spaces="$(echo $i | awk -F: '{print $2}' | sed 's/,/ /g')"
+  cf target -o "${org}"
+  cf enable-org-isolation "${org}" "${VOLUME_ISOLATION_SEGMENT}"
+  for space in ${spaces} ; do
+    cf set-space-isolation-segment "${space}" "${VOLUME_ISOLATION_SEGMENT}"
+  done
+done
