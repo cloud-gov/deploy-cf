fa5a153a26340b82f78c9783839d50baab030b31
Jon Carmack <jonathan.carmack@gsa.gov>
2018-12-18 16:43:05 -0500

    Retrieves expiry dates and names, use this to check for rotation

--- ci/rotate-certs.sh
+++ ci/rotate-certs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-for CA_CERT in `credhub curl -X GET --path "/api/v1/certificates" | jq '.certificates[] | select(.name | endswith( "_ca"))' | .name`; do
+for CA_CERT in `credhub curl -X GET --path "/api/v1/certificates" | jq '.certificates[] | select(.name | endswith( "_ca")) | .name'`; do
     #check cert expiry_date, if expire in 30 days, rotate
-    echo $CA_CERT
+    cert=`echo "$CA_CERT" | sed -e 's/^"//' -e 's/"$//'`
+    curl -s -k -H "Content-Type: application/json" -H "Authorization: $(credhub --token)" $(echo $CREDHUB_SERVER)v1/data?name=$(echo $cert)\&current=true | jq '.data[0].expiry_date'
 done
 export id=$(curl -k -H "Content-Type: application/json" -H "Authorization: $(credhub --token)" $(echo $CREDHUB_SERVER)v1/certificates?name=$NAME
 | jq)
