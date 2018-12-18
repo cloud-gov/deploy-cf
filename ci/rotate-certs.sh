2bfc9fc519ca1eb12d65ab05ec1a6a0cb42ed0e4
Jon Carmack <jonathan.carmack@gsa.gov>
2018-12-17 20:03:25 -0500

    Added loop to iterate over ca certs, need logic to rotate CAs expiring in specified time period

--- ci/rotate-certs.sh
+++ ci/rotate-certs.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-
+for CA_CERT in `credhub curl -X GET --path "/api/v1/certificates" | jq '.certificates[] | select(.name | endswith( "_ca"))' | .name`; do
+    #check cert expiry_date, if expire in 30 days, rotate
+    echo $CA_CERT
+done
+export id=$(curl -k -H "Content-Type: application/json" -H "Authorization: $(credhub --token)" $(echo $CREDHUB_SERVER)v1/certificates?name=$NAME
+| jq)
