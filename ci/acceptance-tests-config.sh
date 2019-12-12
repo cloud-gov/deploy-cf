88359e6d6b93cf0c1353b43d5a2195b6abacc195
Ben Berry <ben.berry@gsa.gov>
2019-12-11 16:52:07 -0800

    temporarily use generated user

--- ci/acceptance-tests-config.sh
+++ ci/acceptance-tests-config.sh
@@ -6,7 +6,7 @@ cat > integration-config/integration_config.json <<EOF
   "apps_domain": "${APPS_DOMAIN}",
   "admin_user": "${ADMIN_USER}",
   "admin_password": "${ADMIN_PASSWORD}",
-  "use_existing_user": true,
+  "use_existing_user": false,
   "existing_user": "${EXISTING_USER}",
   "existing_user_password": "${EXISTING_USER_PASSWORD}",
   "include_container_networking": true,
