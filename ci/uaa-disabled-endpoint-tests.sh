61e4bab3cf46abeafa5bd60d46027f93f0e4d8d0
Steve Greenberg <steve.greenberg@gsa.gov>
2020-04-14 16:01:54 -0600

    use multiple lines in curl command

--- ci/uaa-disabled-endpoint-tests.sh
+++ ci/uaa-disabled-endpoint-tests.sh
@@ -6,14 +6,26 @@
 set -e
 
 # Endpoint should be disabled (GET): /create_account 
-status_code=$(curl --silent --output /dev/null -w "%{http_code}" ${UAA_URL}/create_account)
+status_code=$(curl --silent \
+  --output /dev/null \
+  -w "%{http_code}" \
+  ${UAA_URL}/create_account)
 if [[ "${status_code}" != "404" ]]; then
   echo "ERROR: Endpoint /create_account returned incorrect status code. Expected: 404, Got: ${status_code}"
   exit 1
 fi
 
 # Endpoint should be disabled (POST): /create_account.do 
-status_code=$(curl -X POST --silent --output /dev/null -w "%{http_code}" ${UAA_URL}/create_account.do --data-urlencode client_id=client-id --data-urlencode redirect_uri=redirect-uri --data-urlencode password=password1 --data-urlencode password_confirmation=password1 --data-urlencode email=test@example.com)
+status_code=$(curl -X POST \
+  --silent \
+  --output /dev/null \
+  -w "%{http_code}" \
+  ${UAA_URL}/create_account.do \
+  --data-urlencode client_id=client-id \
+  --data-urlencode redirect_uri=redirect-uri \
+  --data-urlencode password=password1 \
+  --data-urlencode password_confirmation=password1 \
+  --data-urlencode email=test@example.com)
 if [[ "${status_code}" != "404" ]]; then
   echo "ERROR: POST to endpoint /create_account.do returned incorrect status code. Expected: 404, Got: ${status_code}"
   exit 1
