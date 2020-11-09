fa23f6fbaa62574c283cbe99db6207762855abbf
ChrisMcGowan <christopher.r.mcgowan@gsa.gov>
2020-11-09 09:32:14 -0500

    script try 2

--- ci/enable-cf-features.sh
+++ ci/enable-cf-features.sh
@@ -1,31 +1,4 @@
-# #!/bin/sh
-# 
-# set -eux
-# 
-# cf api "${CF_API_URL}"
-# (set +x; cf auth "${CF_USERNAME}" "${CF_PASSWORD}")
-# 
-# # Create isolation segment for platform applications
-# cf target -o "${PLATFORM_ORGANIZATION}"
-# cf create-isolation-segment "${PLATFORM_ISOLATION_SEGMENT}"
-# cf enable-org-isolation "${PLATFORM_ORGANIZATION}" "${PLATFORM_ISOLATION_SEGMENT}"
-# cf set-space-isolation-segment "${PLATFORM_SPACE}" "${PLATFORM_ISOLATION_SEGMENT}"
-# 
-# # Create isolation segment for volume-enabled applications that want to use volume services
-# # VOLUME_TARGETS should be something like "org:space1,space2 org1:space3"
-# cf create-isolation-segment "${VOLUME_ISOLATION_SEGMENT}"
-# for i in ${VOLUME_TARGETS} ; do
-#   org="$(echo $i | awk -F: '{print $1}')"
-#   spaces="$(echo $i | awk -F: '{print $2}' | sed 's/,/ /g')"
-#   cf target -o "${org}"
-#   cf enable-org-isolation "${org}" "${VOLUME_ISOLATION_SEGMENT}"
-#   for space in ${spaces} ; do
-#     cf set-space-isolation-segment "${space}" "${VOLUME_ISOLATION_SEGMENT}"
-#   done
-# done
-
 #!/bin/bash -exu
-
 
 function set_enabled_feature_flags() {
   if [ ! -z "${ENABLED_FEATURE_FLAGS}" ]; then
