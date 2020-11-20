88a515f5c402a537e5e19a230976323bd4983097
Van Nguyen <vanh.nguyen@gsa.gov>
2020-11-20 11:50:17 -0500

    added curl to see timestuff

--- ci/enable-cf-features.sh
+++ ci/enable-cf-features.sh
@@ -26,6 +26,7 @@ function set_feature_flag() {
 
 function main() {
   cf api "${CF_API_URL}"
+  curl -L --output /dev/null --silent --show-error --write-out 'lookup:        %{time_namelookup}\nconnect:       %{time_connect}\nappconnect:    %{time_appconnect}\npretransfer:   %{time_pretransfer}\nredirect:      %{time_redirect}\nstarttransfer: %{time_starttransfer}\ntotal:         %{time_total}\n' 'https://login.dev.us-gov-west-1.aws-us-gov.cloud.gov/oauth/token'
   (set -x; CF_TRACE=true cf auth "${CF_USERNAME}" "${CF_PASSWORD}")
 
   set_enabled_feature_flags
