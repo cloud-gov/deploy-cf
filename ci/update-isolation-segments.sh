fa8665bf9633772ee35243098d2dc867358102bb
Van Nguyen <vanh.nguyen@gsa.gov>
2020-11-20 11:07:47 -0500

    Enabling CF trace

--- ci/update-isolation-segments.sh
+++ ci/update-isolation-segments.sh
@@ -2,11 +2,11 @@
 
 set -eux
 
-cf api "${CF_API_URL}"
-(set +x; cf auth "${CF_USERNAME}" "${CF_PASSWORD}")
+CF_TRACE=true cf api "${CF_API_URL}"
+(set +x; CF_TRACE=true cf auth "${CF_USERNAME}" "${CF_PASSWORD}")
 
 # Create isolation segment for platform applications
-cf target -o "${PLATFORM_ORGANIZATION}"
+CF_TRACE=true cf target -o "${PLATFORM_ORGANIZATION}"
 cf create-isolation-segment "${PLATFORM_ISOLATION_SEGMENT}"
 cf enable-org-isolation "${PLATFORM_ORGANIZATION}" "${PLATFORM_ISOLATION_SEGMENT}"
 cf set-space-isolation-segment "${PLATFORM_SPACE}" "${PLATFORM_ISOLATION_SEGMENT}"
