e284ab3f6130c6e43236c186ac66c02bfb66c7c2
Van Nguyen <vhnguyen326@gmail.com>
2021-04-19 10:41:59 -0400

    Update asg.tf
    MOAR RAM

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -172,7 +172,7 @@ resource "cloudfoundry_asg" "smtp" {
 resource "cloudfoundry_org_quota" "default-tts" {
   name                     = "default-tts"
   allow_paid_service_plans = true
-  total_memory             = 40960
+  total_memory             = 81920
   total_routes             = 1000
   total_services           = 200
   total_route_ports        = -1
