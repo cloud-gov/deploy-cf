54dd88175aae6b0b5876eaa2d472569289426312
Andrew Burnes <andrew.burnes@gsa.gov>
2021-10-05 08:21:01 -0700

    Add trap to cleanup on error

--- ci/test-space-egress/run.sh
+++ ci/test-space-egress/run.sh
@@ -4,12 +4,22 @@
 
 set -e
 
+# Clean up deployment if an error occurs
+onerr() {
+  ./clean.sh
+  exit 1
+}
 
+trap 'onerr' ERR
+
+# Deploy the org, spaces, and apps
 echo "Deploying test environment"
 ./deploy.sh
 
+# Run the tests against the endpoint
 echo "Running tests"
 ./run-tests.sh
 
+# Cleanup and remove the apps, spaces, and org
 echo "Cleaning up test environment"
 ./clean.sh
