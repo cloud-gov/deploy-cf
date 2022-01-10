720fa888cf52af5733f5d991b6a27c00c4507c13
Andrew Burnes <andrew.burnes@gsa.gov>
2022-01-10 16:08:11 -0700

    Add recaptcha_egress asg with ip ranges

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -242,6 +242,297 @@ resource "cloudfoundry_asg" "smtp" {
   }
 }
 
+# Google reCAPTCHA IPranges.
+
+resource "cloudfoundry_asg" "recaptcha_egress" {
+  name = "recaptcha_egress"
+
+  rule {
+    protocol    = "all"
+    destination = "8.8.4.0/24"
+  }
+  rule {
+    protocol    = "all"
+    destination = "8.8.8.0/24"
+  }
+  rule {
+    protocol    = "all"
+    destination = "8.34.208.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "8.35.192.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "23.236.48.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "23.251.128.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "34.64.0.0/10"
+  }
+  rule {
+    protocol    = "all"
+    destination = "34.128.0.0/10"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.184.0.0/13"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.192.0.0/14"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.196.0.0/15"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.198.0.0/16"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.199.0.0/17"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.199.128.0/18"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.200.0.0/13"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.208.0.0/12"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.224.0.0/12"
+  }
+  rule {
+    protocol    = "all"
+    destination = "35.240.0.0/13"
+  }
+  rule {
+    protocol    = "all"
+    destination = "64.15.112.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "64.233.160.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "66.102.0.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "66.249.64.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "70.32.128.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "72.14.192.0/18"
+  }
+  rule {
+    protocol    = "all"
+    destination = "74.114.24.0/21"
+  }
+  rule {
+    protocol    = "all"
+    destination = "74.125.0.0/16"
+  }
+  rule {
+    protocol    = "all"
+    destination = "104.154.0.0/15"
+  }
+  rule {
+    protocol    = "all"
+    destination = "104.196.0.0/14"
+  }
+  rule {
+    protocol    = "all"
+    destination = "104.237.160.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "107.167.160.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "107.178.192.0/18"
+  }
+  rule {
+    protocol    = "all"
+    destination = "108.59.80.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "108.170.192.0/18"
+  }
+  rule {
+    protocol    = "all"
+    destination = "108.177.0.0/17"
+  }
+  rule {
+    protocol    = "all"
+    destination = "130.211.0.0/16"
+  }
+  rule {
+    protocol    = "all"
+    destination = "136.112.0.0/12"
+  }
+  rule {
+    protocol    = "all"
+    destination = "142.250.0.0/15"
+  }
+  rule {
+    protocol    = "all"
+    destination = "146.148.0.0/17"
+  }
+  rule {
+    protocol    = "all"
+    destination = "162.216.148.0/22"
+  }
+  rule {
+    protocol    = "all"
+    destination = "162.222.176.0/21"
+  }
+  rule {
+    protocol    = "all"
+    destination = "172.110.32.0/21"
+  }
+  rule {
+    protocol    = "all"
+    destination = "172.217.0.0/16"
+  }
+  rule {
+    protocol    = "all"
+    destination = "172.253.0.0/16"
+  }
+  rule {
+    protocol    = "all"
+    destination = "173.194.0.0/16"
+  }
+  rule {
+    protocol    = "all"
+    destination = "173.255.112.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "192.158.28.0/22"
+  }
+  rule {
+    protocol    = "all"
+    destination = "192.178.0.0/15"
+  }
+  rule {
+    protocol    = "all"
+    destination = "193.186.4.0/24"
+  }
+  rule {
+    protocol    = "all"
+    destination = "199.36.154.0/23"
+  }
+  rule {
+    protocol    = "all"
+    destination = "199.36.156.0/24"
+  }
+  rule {
+    protocol    = "all"
+    destination = "199.192.112.0/22"
+  }
+  rule {
+    protocol    = "all"
+    destination = "199.223.232.0/21"
+  }
+  rule {
+    protocol    = "all"
+    destination = "207.223.160.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "208.65.152.0/22"
+  }
+  rule {
+    protocol    = "all"
+    destination = "208.68.108.0/22"
+  }
+  rule {
+    protocol    = "all"
+    destination = "208.81.188.0/22"
+  }
+  rule {
+    protocol    = "all"
+    destination = "208.117.224.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "209.85.128.0/17"
+  }
+  rule {
+    protocol    = "all"
+    destination = "216.58.192.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "216.73.80.0/20"
+  }
+  rule {
+    protocol    = "all"
+    destination = "216.239.32.0/19"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2001:4860::/32"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2404:6800::/32"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2404:f340::/32"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2600:1900::/28"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2606:73c0::/32"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2607:f8b0::/32"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2620:11a:a000::/40"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2620:120:e000::/40"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2a00:1450::/32"
+  }
+  rule {
+    protocol    = "all"
+    destination = "2c0f:fb50::/32"
+  }
+}
+
 # Default global running ASG
 resource "cloudfoundry_default_asg" "running" {
     name = "running"
