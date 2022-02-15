c8fdcf0cbe521faa2a1d23c53f99237cdf63092d
Ben Berry <ben.berry@gsa.gov>
2022-02-15 12:22:21 -0800

    the copy-pasta continues

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -279,7 +279,7 @@ resource "cloudfoundry_isolation_segment" "platform" {
 resource "cloudfoundry_isolation_segment_entitlement" "platform" {
   segment = cloudfoundry_isolation_segment.platform.id
   orgs = [
-    data.cloudfoundry_org.cloud-gov.id
+    cloudfoundry_org.cloud-gov.id
   ]
 }
 
