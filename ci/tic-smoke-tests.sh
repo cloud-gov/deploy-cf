6a7f0ca12f6517a2608dc8482eed6c4991cb192f
Ben Berry <ben.berry@gsa.gov>
2022-05-12 15:05:41 -0700

    Add specific dashboard tic tests
    Stratos needs different targets and expected responses for test successes, so we need dashboard-specific tests
    - Add specific dashboard tic tests
    - Also update tests to have more human-readable names
    - Add comments about why specific responses are expected

--- ci/tic-smoke-tests.sh
+++ ci/tic-smoke-tests.sh
@@ -16,18 +16,20 @@ gorouter_ip=$(
     | head -n 1
 )
 
-@test "restricted user | address allowed" {
+@test "restricted user from allowed address can reach API" {
   resp=$(curl -s -k \
     ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/v2/apps \
     -H "Host: ${API_HOSTNAME}" \
     -H "Authorization: $(echo '{}' | base64).$(echo ${restricted_payload} | base64).$(echo '{}' | base64)" \
     -H "X-Forwarded-For: ${SOURCE_ADDRESS_ALLOWED}" \
     -w '\n%{http_code}')
+  # 401 because our Authorization header is bogus
   [ $(echo "${resp}" | tail -n 1) = "401" ]
+  # validate the code in the body to make sure it looks like we really reached CAPI
   [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "10002" ]
 }
 
-@test "restricted user | address forbidden" {
+@test "restricted user from unallowed is blocked by secureproxy when requesting API" {
   resp=$(curl -s -k \
     ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/v2/apps \
     -H "Host: ${API_HOSTNAME}" \
@@ -38,7 +40,7 @@ gorouter_ip=$(
   [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
 }
 
-@test "restricted user | secret allowed | proxy allowed | address allowed (X-Client-IP)" {
+@test "x-client-ip is trusted when x-forwarded-for is a trusted proxy, allowing restricted user to access API from allowed address" {
   resp=$(curl -s -k \
     ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/v2/apps \
     -H "Host: ${API_HOSTNAME}" \
@@ -47,11 +49,13 @@ gorouter_ip=$(
     -H "X-TIC-Secret: ${TIC_SECRET_ALLOWED}" \
     -H "X-Client-IP: ${SOURCE_ADDRESS_ALLOWED}" \
     -w '\n%{http_code}')
+  # 401 because our Authorization header is bogus
   [ $(echo "${resp}" | tail -n 1) = "401" ]
+  # validate the code in the body to make sure it looks like we really reached CAPI
   [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "10002" ]
 }
 
-@test "restricted user | secret allowed | proxy forbidden | address allowed (X-Client-IP)" {
+@test "x-client-ip is not trusted when x-forwarded-for is untrusted, disallowing restricted user from accessing API with allowed address in x-client-ip" {
   resp=$(curl -s -k \
     ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/v2/apps \
     -H "Host: ${API_HOSTNAME}" \
@@ -64,7 +68,7 @@ gorouter_ip=$(
   [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
 }
 
-@test "restricted user | secret forbidden | proxy allowed | address allowed (X-Client-IP)" {
+@test "x-client-ip is not trusted when x-tic-secret is invalid, disallowing restricted user from accessing API with allowed address in x-client-ip" {
   resp=$(curl -s -k \
     ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/v2/apps \
     -H "Host: ${API_HOSTNAME}" \
@@ -77,7 +81,7 @@ gorouter_ip=$(
   [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
 }
 
-@test "restricted user | secret allowed | proxy allowed | address forbidden (X-Client-IP)" {
+@test "x-client-ip is trusted when x-tic-secret and proxy address are trusted, disallowing restricted user from accessing API with disallowed address in x-client-ip" {
   resp=$(curl -s -k \
     ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/v2/apps \
     -H "Host: ${API_HOSTNAME}" \
@@ -90,7 +94,7 @@ gorouter_ip=$(
   [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
 }
 
-@test "restricted user | secret empty | proxy allowed | address allowed (X-Client-IP)" {
+@test "x-client-ip is not trusted when x-tic-secret is empty, disallowing restricted user from accessing API with allowed address in x-client-ip" {
   resp=$(curl -s -k \
     ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/v2/apps \
     -H "Host: ${API_HOSTNAME}" \
@@ -103,13 +107,119 @@ gorouter_ip=$(
   [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
 }
 
-@test "unrestricted user" {
+@test "unrestricted user can access API from forbidden address" {
   resp=$(curl -s -k \
     ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/v2/apps \
     -H "Host: ${API_HOSTNAME}" \
     -H "Authorization: $(echo '{}' | base64).$(echo ${unrestricted_payload} | base64).$(echo '{}' | base64)" \
     -H "X-Forwarded-For: ${SOURCE_ADDRESS_FORBIDDEN}" \
     -w '\n%{http_code}')
+  # 401 because our Authorization header is bogus
   [ $(echo "${resp}" | tail -n 1) = "401" ]
+  # validate the code in the body to make sure it looks like we really reached CAPI
   [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "10002" ]
+}
+
+@test "restricted user can access the dashboard from an allowed address" {
+  resp=$(curl -s -k \
+    ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/pp/v1/proxy/v2/apps \
+    -H "Host: ${DASHBOARD_HOSTNAME}" \
+    -H "Authorization: $(echo '{}' | base64).$(echo ${restricted_payload} | base64).$(echo '{}' | base64)" \
+    -H "X-Forwarded-For: ${SOURCE_ADDRESS_ALLOWED}" \
+    -w '\n%{http_code}')
+  # 401 because our Authorization header is bogus
+  [ $(echo "${resp}" | tail -n 1) = "401" ]
+  # validate the error message in the body to make sure it looks like we really reached Stratos
+  [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "User session could not be found" ]
+}
+
+@test "restricted user cannot access the dashboard from an unallowed address" {
+  resp=$(curl -s -k \
+    ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/pp/v1/proxy/v2/apps \
+    -H "Host: ${DASHBOARD_HOSTNAME}" \
+    -H "Authorization: $(echo '{}' | base64).$(echo ${restricted_payload} | base64).$(echo '{}' | base64)" \
+    -H "X-Forwarded-For: ${SOURCE_ADDRESS_FORBIDDEN}" \
+    -w '\n%{http_code}')
+  [ $(echo "${resp}" | tail -n 1) = "403" ]
+  [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
+}
+
+@test "x-client-ip is trusted when x-forwarded-for is a trusted proxy, allowing restricted user to access dashboard from allowed address" {
+  resp=$(curl -s -k \
+    ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/pp/v1/proxy/v2/apps \
+    -H "Host: ${DASHBOARD_HOSTNAME}" \
+    -H "Authorization: $(echo '{}' | base64).$(echo ${restricted_payload} | base64).$(echo '{}' | base64)" \
+    -H "X-Forwarded-For: ${PROXY_ADDRESS_ALLOWED}" \
+    -H "X-TIC-Secret: ${TIC_SECRET_ALLOWED}" \
+    -H "X-Client-IP: ${SOURCE_ADDRESS_ALLOWED}" \
+    -w '\n%{http_code}')
+  # 401 because our Authorization header is bogus
+  [ $(echo "${resp}" | tail -n 1) = "401" ]
+  # validate the code in the body to make sure it looks like we really reached Stratos
+  [ $(echo "${resp}" | head -n -1 | jq -r '.error') = "User session could not be found" ]
+}
+
+@test "x-client-ip is not trusted when x-forwarded-for is untrusted, disallowing restricted user from accessing dashboard with allowed address in x-client-ip" {
+  resp=$(curl -s -k \
+    ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/pp/v1/proxy/v2/apps \
+    -H "Host: ${DASHBOARD_HOSTNAME}" \
+    -H "Authorization: $(echo '{}' | base64).$(echo ${restricted_payload} | base64).$(echo '{}' | base64)" \
+    -H "X-Forwarded-For: ${PROXY_ADDRESS_FORBIDDEN}" \
+    -H "X-TIC-Secret: ${TIC_SECRET_ALLOWED}" \
+    -H "X-Client-IP: ${SOURCE_ADDRESS_ALLOWED}" \
+    -w '\n%{http_code}')
+  [ $(echo "${resp}" | tail -n 1) = "403" ]
+  [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
+}
+
+@test "x-client-ip is not trusted when x-tic-secret is invalid, disallowing restricted user from accessing dashboard with allowed address in x-client-ip" {
+  resp=$(curl -s -k \
+    ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/pp/v1/proxy/v2/apps \
+    -H "Host: ${DASHBOARD_HOSTNAME}" \
+    -H "Authorization: $(echo '{}' | base64).$(echo ${restricted_payload} | base64).$(echo '{}' | base64)" \
+    -H "X-Forwarded-For: ${PROXY_ADDRESS_ALLOWED}" \
+    -H "X-TIC-Secret: ${TIC_SECRET_FORBIDDEN}" \
+    -H "X-Client-IP: ${SOURCE_ADDRESS_ALLOWED}" \
+    -w '\n%{http_code}')
+  [ $(echo "${resp}" | tail -n 1) = "403" ]
+  [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
+}
+
+@test "x-client-ip is trusted when x-tic-secret and proxy address are trusted, disallowing restricted user from accessing dashboard with disallowed address in x-client-ip" {
+  resp=$(curl -s -k \
+    ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/pp/v1/proxy/v2/apps \
+    -H "Host: ${DASHBOARD_HOSTNAME}" \
+    -H "Authorization: $(echo '{}' | base64).$(echo ${restricted_payload} | base64).$(echo '{}' | base64)" \
+    -H "X-Forwarded-For: ${PROXY_ADDRESS_ALLOWED}" \
+    -H "X-TIC-Secret: ${TIC_SECRET_ALLOWED}" \
+    -H "X-Client-IP: ${SOURCE_ADDRESS_FORBIDDEN}" \
+    -w '\n%{http_code}')
+  [ $(echo "${resp}" | tail -n 1) = "403" ]
+  [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
+}
+
+@test "x-client-ip is not trusted when x-tic-secret is empty, disallowing restricted user from accessing dashboard with allowed address in x-client-ip" {
+  resp=$(curl -s -k \
+    ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/pp/v1/proxy/v2/apps \
+    -H "Host: ${DASHBOARD_HOSTNAME}" \
+    -H "Authorization: $(echo '{}' | base64).$(echo ${restricted_payload} | base64).$(echo '{}' | base64)" \
+    -H "X-Forwarded-For: ${PROXY_ADDRESS_ALLOWED}" \
+    -H "X-TIC-Secret: " \
+    -H "X-Client-IP: ${SOURCE_ADDRESS_ALLOWED}" \
+    -w '\n%{http_code}')
+  [ $(echo "${resp}" | tail -n 1) = "403" ]
+  [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "9162" ]
+}
+
+@test "unrestricted user can access the dashboard from unallowed address" {
+  resp=$(curl -s -k \
+    ${TIC_PROTOCAL}://${gorouter_ip}:${GOROUTER_PORT:-443}/pp/v1/proxy/v2/apps \
+    -H "Host: ${DASHBOARD_HOSTNAME}" \
+    -H "Authorization: $(echo '{}' | base64).$(echo ${unrestricted_payload} | base64).$(echo '{}' | base64)" \
+    -H "X-Forwarded-For: ${SOURCE_ADDRESS_FORBIDDEN}" \
+    -w '\n%{http_code}')
+  # 401 because our Authorization header is bogus
+  [ $(echo "${resp}" | tail -n 1) = "401" ]
+  # validate the error message in the body to make sure it looks like we really reached Stratos
+  [ $(echo "${resp}" | head -n -1 | jq -r '.code') = "User session could not be found" ]
 }
