7c4b44bc0b4bc6abe519ef00f6436a1233e38a34
Christopher Weibel <christopher.weibel@gsa.gov>
2023-08-29 13:29:23 -0400

    Add ops files for uaa split

---
+++ ci/create-router-main.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eux
+
+## Extract current base configuration for the router instance group from upstream
+bosh int cf-deployment/cf-deployment.yml --path /instance_groups/name=router > router_raw.yml
+
+## Create ops file header
+cat > router_main.yml <<EOF
+- type: replace
+  path: /instance_groups/name=router_main?
+  value:
+EOF
+
+## Replace name of instance group and indent 4 spaces
+sed 's/name: router/name: router_main/' router_raw.yml > router_name.yml
+sed 's/^/    /' router_name.yml > router_indented.yml
+
+
+## Append the router yaml to the ops file header
+cat router_indented.yml >> router_main.yml
+
+## return: router_main.yml
+
+## TEST ONLY: Will it float?
+#bosh int cf-deployment/cf-deployment.yml -o router_main.yml > router_blah.yml
