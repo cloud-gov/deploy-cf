6df41870b49616f47b93dc3104aa06a6c1f4e218
Stephen Greenberg <stephen.greenberg@gsa.gov>
2024-06-04 13:41:22 -0600

    provide empty defaults

--- terraform/stack/cloudfoundry/vpc.tf
+++ terraform/stack/cloudfoundry/vpc.tf
@@ -10,8 +10,8 @@ module "vpc" {
   restricted_ingress_web_cidrs      = var.restricted_ingress_web_cidrs
   restricted_ingress_web_ipv6_cidrs = var.restricted_ingress_web_ipv6_cidrs
   nat_gateway_instance_type         = var.nat_gateway_instance_type
-  monitoring_security_group_cidrs   = var.target_monitoring_security_group_cidrs
-  concourse_security_group_cidrs    = var.target_concourse_security_group_cidrs
+  monitoring_security_group_cidrs   = []
+  concourse_security_group_cidrs    = []
   bosh_default_ssh_public_key       = var.bosh_default_ssh_public_key
   s3_gateway_policy_accounts        = var.s3_gateway_policy_accounts
 }
