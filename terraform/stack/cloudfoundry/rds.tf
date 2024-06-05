4a0cd6a05d8aacd1e89d1c7ca224c7786adfab64
Stephen Greenberg <stephen.greenberg@gsa.gov>
2024-06-05 12:36:25 -0600

    create db

--- terraform/stack/cloudfoundry/rds.tf
+++ terraform/stack/cloudfoundry/rds.tf
@@ -44,21 +44,21 @@ module "rds_network" {
   route_table_ids       = module.vpc.private_route_table_ids
 }
 
-# module "cfdb" {
-#   source = "github.com/cloud-gov/cg-provision//terraform/modules/rds?ref=f140"
+module "cfdb" {
+  source = "github.com/cloud-gov/cg-provision//terraform/modules/rds?ref=f140"
 
-#   stack_description               = "${var.env_name}-cfdb"
-#   rds_instance_type               = var.rds_instance_type
-#   rds_db_size                     = var.rds_db_size
-#   rds_db_engine                   = var.rds_db_engine
-#   rds_db_engine_version           = var.rds_db_engine_version
-#   rds_db_name                     = var.rds_db_name
-#   rds_username                    = var.rds_username
-#   rds_password                    = var.rds_password
-#   rds_subnet_group                = module.rds_network.rds_subnet_group
-#   rds_security_groups             = [module.rds_network.rds_postgres_security_group]
-#   rds_parameter_group_family      = var.rds_parameter_group_family
-#   rds_allow_major_version_upgrade = var.rds_allow_major_version_upgrade
-#   rds_apply_immediately           = var.rds_apply_immediately
-# }
+  stack_description               = "${var.env_name}-cfdb"
+  rds_instance_type               = var.rds_instance_type
+  rds_db_size                     = var.rds_db_size
+  rds_db_engine                   = var.rds_db_engine
+  rds_db_engine_version           = var.rds_db_engine_version
+  rds_db_name                     = var.rds_db_name
+  rds_username                    = var.rds_username
+  rds_password                    = var.rds_password
+  rds_subnet_group                = module.rds_network.rds_subnet_group
+  rds_security_groups             = [module.rds_network.rds_postgres_security_group]
+  rds_parameter_group_family      = var.rds_parameter_group_family
+  rds_allow_major_version_upgrade = var.rds_allow_major_version_upgrade
+  rds_apply_immediately           = var.rds_apply_immediately
+}
 
