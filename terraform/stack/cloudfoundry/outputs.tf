3b03df9c5dafb7ea294902bb017ab1291b0cdae0
Stephen Greenberg <stephen.greenberg@gsa.gov>
2024-06-07 11:42:04 -0600

    duplication

--- terraform/stack/cloudfoundry/outputs.tf
+++ terraform/stack/cloudfoundry/outputs.tf
@@ -15,10 +15,6 @@ output "cf_rds_username" {
   value = module.cfdb.rds_username
 }
 
-output "cf_rds_username" {
-  value = module.cfdb.rds_username
-}
-
 output "tcp_lb_listener_ports" {
   value = aws_lb_listener.cf_apps_tcp.*.port
 }
