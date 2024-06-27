61221ce80a862a0a5518d27791acfb4acf283967
Christopher Weibel <christopher.weibel@gsa.gov>
2024-06-27 10:47:49 -0400

    Switch to list of names for iso seg, add -

--- ci/create-diego-cell-iso-seg.sh
+++ ci/create-diego-cell-iso-seg.sh
@@ -27,7 +27,7 @@ for iso_seg_name in $ISO_SEG_NAMES; do
   echo "Creating isolation segment ${iso_seg_name}"...
 
   ## Create ops file header - Always start with the instance group declaration
-  cat > diego-cell-iso-seg${iso_seg_name}-header.yml <<EOF
+  cat > diego-cell-iso-seg-${iso_seg_name}-header.yml <<EOF
 
 # Add iso seg ${iso_seg_name} instance group
 - type: replace
@@ -36,46 +36,46 @@ for iso_seg_name in $ISO_SEG_NAMES; do
 EOF
 
   ## Create ops file body - replace name of instance group, swap out provides/consumes values and indent 4 spaces
-  sed "s/name: diego-cell/name: diego-cell-iso-seg${iso_seg_name}/" diego-cell_raw.yml > sed1.yml
-  sed "s/iptables-tenant/iptables-iso-seg${iso_seg_name}/" sed1.yml > sed2.yml
-  sed "s/cni_config_tenant/cni_config_iso-seg${iso_seg_name}/" sed2.yml > sed3.yml
-  sed "s/vpa-tenant/vpa-iso-seg${iso_seg_name}/" sed3.yml > sed4.yml
-  sed 's/^/    /' sed4.yml > diego-cell_indented-iso-seg${iso_seg_name}.yml
+  sed "s/name: diego-cell/name: diego-cell-iso-seg-${iso_seg_name}/" diego-cell_raw.yml > sed1.yml
+  sed "s/iptables-tenant/iptables-iso-seg-${iso_seg_name}/" sed1.yml > sed2.yml
+  sed "s/cni_config_tenant/cni_config_iso-seg-${iso_seg_name}/" sed2.yml > sed3.yml
+  sed "s/vpa-tenant/vpa-iso-seg-${iso_seg_name}/" sed3.yml > sed4.yml
+  sed 's/^/    /' sed4.yml > diego-cell_indented-iso-seg-${iso_seg_name}.yml
 
   ## Create ops file footer - All the "replace" that can only be run once the instance group exists (order matters)
-  cat > diego-cell-iso-seg${iso_seg_name}-footer.yml <<EOF
+  cat > diego-cell-iso-seg-${iso_seg_name}-footer.yml <<EOF
 
 # Add iso seg ${iso_seg_name} placement tag
 - type: replace
-  path: /instance_groups/name=diego-cell-iso-seg${iso_seg_name}/jobs/name=rep/properties/diego/rep/placement_tags?/-
-  value: diego-cell-iso-seg${iso_seg_name}
+  path: /instance_groups/name=diego-cell-iso-seg-${iso_seg_name}/jobs/name=rep/properties/diego/rep/placement_tags?/-
+  value: diego-cell-iso-seg-${iso_seg_name}
 
 # Add iso seg ${iso_seg_name} to DNS aliases
 - type: replace
   path: /addons/name=bosh-dns-aliases/jobs/name=bosh-dns-aliases/properties/aliases/domain=_.cell.service.cf.internal/targets/-
   value:
     query: '_'
-    instance_group: diego-cell-iso-seg${iso_seg_name}
+    instance_group: diego-cell-iso-seg-${iso_seg_name}
     deployment: ((deployment_name))
     network: ((network_name))
     domain: bosh
 
 # Set default instance type since the one upstream has doesn't exists. Override this in scaling-*.yml
 - type: replace
-  path: /instance_groups/name=diego-cell-iso-seg${iso_seg_name}/vm_type
+  path: /instance_groups/name=diego-cell-iso-seg-${iso_seg_name}/vm_type
   value: t3.xlarge
 
 # Start with 2 instances.  Override this in scaling-*.yml
 - type: replace
-  path: /instance_groups/name=diego-cell-iso-seg${iso_seg_name}/instances
+  path: /instance_groups/name=diego-cell-iso-seg-${iso_seg_name}/instances
   value: 2
 EOF
 
   ## Append the header, main, and footer for this iso-seg
-  cat diego-cell-iso-seg${iso_seg_name}-header.yml diego-cell_indented-iso-seg${iso_seg_name}.yml diego-cell-iso-seg${iso_seg_name}-footer.yml > diego-cell-iso-seg${iso_seg_name}.yml
+  cat diego-cell-iso-seg-${iso_seg_name}-header.yml diego-cell_indented-iso-seg-${iso_seg_name}.yml diego-cell-iso-seg-${iso_seg_name}-footer.yml > diego-cell-iso-seg-${iso_seg_name}.yml
 
   ## Merge this iso-seg into one file which will have all of them at the end of the loop
-  cat diego-cell-iso-seg${iso_seg_name}.yml >> diego-cell-iso-seg.yml
+  cat diego-cell-iso-seg-${iso_seg_name}.yml >> diego-cell-iso-seg.yml
 done
 
 ## Either return the iso-seg file or a comment only file so "bosh deploy" will work in the main pipeline
