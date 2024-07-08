5720af664c9c16190ad2a653f5f46b0891dd79f0
Mark Boyd <mark.boyd@gsa.gov>
2024-07-08 15:39:56 -0400

    break up TF code into named files

---
+++ terraform/stack/orgs.tf
@@ -0,0 +1,10 @@
+resource "cloudfoundry_org" "cloud-gov" {
+  name  = "cloud-gov"
+  quota = cloudfoundry_org_quota.default-tts.id
+}
+
+# Federalist/Pages
+
+data "cloudfoundry_org" "gsa-18f-federalist" {
+  name  = "gsa-18f-federalist"
+}
