dca4ec46329de770afe7b1d5168e6bb2fe0dda2d
James Hochadel <james.hochadel@gsa.gov>
2024-09-16 17:23:24 -0400

    .result is required to access password

--- terraform/modules/csb/main.tf
+++ terraform/modules/csb/main.tf
@@ -44,7 +44,7 @@ resource "cloudfoundry_app" "csb" {
   environment = {
     # General broker configuration
     SECURITY_USER_NAME         = "broker"
-    SECURITY_USER_PASSWORD     = random_password.csb_app_password
+    SECURITY_USER_PASSWORD     = random_password.csb_app_password.result
     TERRAFORM_UPGRADES_ENABLED = true
     BROKERPAK_UPDATES_ENABLED  = true
 
