d97453b5ff11f024ae8cd5994bae96b3ae5c63fa
James Hochadel <james.hochadel@gsa.gov>
2024-10-10 11:17:39 -0400

    Create new apps stack with contents of 8526d97

---
+++ terraform/stacks/apps/variables.tf
@@ -0,0 +1,63 @@
+variable "stack_name" {
+  type        = string
+  description = "One of development, staging, production."
+}
+
+variable "remote_state_bucket" {
+  type = string
+}
+
+variable "remote_state_bucket_external" {
+  type = string
+}
+
+variable "external_remote_state_reader_access_key_id" {
+  type        = string
+  description = "Access key ID for the IAM user that has permission to read from the state bucket."
+}
+
+variable "external_remote_state_reader_secret_access_key" {
+  type        = string
+  sensitive   = true
+  description = "Secret access key for the IAM user that has permission to read from the state bucket."
+}
+
+variable "external_remote_state_reader_region" {
+  type        = string
+  description = "The region in which the remote state bucket is located."
+}
+
+variable "external_stack_name" {
+  type = string
+}
+
+variable "domain_name" {
+}
+
+variable "csb_aws_region_govcloud" {
+  type = string
+}
+
+variable "csb_aws_region_commercial" {
+  type = string
+}
+
+variable "csb_cg_smtp_aws_ses_zone" {
+  type = string
+}
+
+variable "csb_docker_image_name" {
+  type = string
+}
+
+variable "csb_org_name" {
+  type = string
+}
+
+variable "csb_space_name" {
+  type = string
+}
+
+variable "csb_broker_route_domain" {
+  type = string
+}
