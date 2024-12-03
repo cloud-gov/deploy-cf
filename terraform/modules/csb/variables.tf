b3133ce40474caff923cda7a4d2ad29d9c4634d5
James Hochadel <james.hochadel@gsa.gov>
2024-12-03 10:30:25 -0500

    Remove resources from cf-community provider to prepare for new provider

--- terraform/modules/csb/variables.tf
+++ terraform/modules/csb/variables.tf
@@ -3,6 +3,50 @@ variable "stack_name" {
   description = "Like development, staging, or production."
 }
 
+# CSB CF Application Configuration
+
+variable "org_name" {
+  type        = string
+  description = "The name of the Cloud Foundry organization in which the broker will be deployed."
+}
+
+variable "space_name" {
+  type        = string
+  description = "The name of the Cloud Foundry space in which the broker will be deployed."
+}
+
+variable "docker_image_name" {
+  type        = string
+  description = "Full name (but not tag or SHA) of the Docker image the broker will use."
+}
+
+variable "docker_image_version" {
+  type        = string
+  description = "Tag or SHA of the Docker image the broker will use. For example, ':latest' or '@sha256:abc123...'."
+  default     = ":latest"
+}
+
+variable "ecr_access_key_id" {
+  description = "For pulling the CSB image from ECR."
+  type        = string
+}
+
+variable "ecr_secret_access_key" {
+  description = "For pulling the CSB image from ECR."
+  sensitive   = true
+  type        = string
+}
+
+variable "instances" {
+  description = "Number of instances of the CSB app to run."
+  type        = number
+}
+
+variable "broker_route_domain" {
+  type        = string
+  description = "The domain under which the broker's route will be created. For example, 'fr.cloud.gov'."
+}
+
 # Database credentials
 
 variable "rds_host" {
@@ -31,30 +75,13 @@ variable "rds_password" {
   description = "Database password of the RDS instance for the Cloud Service Broker."
 }
 
-# Application variables
-
-variable "ecr_access_key_id" {
-  description = "For pulling the CSB image from ECR."
-  type        = string
-}
-
-variable "ecr_secret_access_key" {
-  description = "For pulling the CSB image from ECR."
-  sensitive   = true
-  type        = string
-}
-
-variable "instances" {
-  description = "Number of instances of the CSB app to run."
-  type        = number
-}
+# CSB Configuration
 
 variable "cg_smtp_aws_ses_zone" {
   type        = string
   description = "When the user does not provide a domain, a subdomain will be created for them under this DNS zone."
 }
 
-// Broker credentials
 variable "aws_access_key_id_govcloud" {
   type = string
 }
@@ -79,30 +106,4 @@ variable "aws_secret_access_key_commercial" {
 
 variable "aws_region_commercial" {
   type = string
-}
-
-variable "org_name" {
-  type        = string
-  description = "The name of the Cloud Foundry organization in which the broker will be deployed."
-}
-
-variable "space_name" {
-  type        = string
-  description = "The name of the Cloud Foundry space in which the broker will be deployed."
-}
-
-variable "docker_image_name" {
-  type        = string
-  description = "Full name (but not tag or SHA) of the Docker image the broker will use."
-}
-
-variable "docker_image_version" {
-  type        = string
-  description = "Tag or SHA of the Docker image the broker will use. For example, ':latest' or '@sha256:abc123...'."
-  default     = ":latest"
-}
-
-variable "broker_route_domain" {
-  type        = string
-  description = "The domain under which the broker's route will be created. For example, 'fr.cloud.gov'."
 }
