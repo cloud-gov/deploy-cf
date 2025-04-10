ae472d58ccf7dc97971a7028898f09f7a18d6a86
Mark Boyd <mark.boyd@gsa.gov>
2025-04-10 15:24:06 -0400

    Revert "add CF user for opensearch e2e test setup (#959)" (#960)
    * Revert "add CF user for opensearch e2e test setup (#959)"
    
    This reverts commit 0aebcdf07e40bc83362316ab6fc81de72c371664.
    
    * pull stray variable

--- terraform/stacks/cf/users.tf
+++
@@ -1,6 +0,0 @@
-resource "cloudfoundry_user" "opensearch_e2e_test_setup_user" {
-  name     = "opensearch-e2e-test-setup-user"
-  password = var.opensearch_e2e_test_setup_user_password
-
-  groups = ["cloud_controller.admin", "scim.read", "scim.write"]
-}
--- terraform/stacks/cf/variables.tf
+++ terraform/stacks/cf/variables.tf
@@ -9,8 +9,3 @@ variable "iaas_stack_name" {
 
 variable "domain_name" {
 }
-
-variable "opensearch_e2e_test_setup_user_password" {
-  type        = string
-  description = "Password for OpenSearch E2E test setup user"
-}
