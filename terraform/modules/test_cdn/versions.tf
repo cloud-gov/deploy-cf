430db263a0abc3f3217d3f9ae0c565c399e5833e
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-04-30 13:41:19 -0600

    remove old provider

--- terraform/modules/test_cdn/versions.tf
+++ terraform/modules/test_cdn/versions.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = "< 2.0.0"
   required_providers {
-    cloudfoundry = {
-      source  = "cloudfoundry-community/cloudfoundry"
-      version = "0.53.0"
-    }
     cloudfoundryv3 = {
       source  = "cloudfoundry/cloudfoundry"
       version = "< 2.0"
--- terraform/stacks/cf/versions.tf
+++ terraform/stacks/cf/versions.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 0.14"
   required_providers {
-    cloudfoundry = {
-      source  = "cloudfoundry-community/cloudfoundry"
-      version = "0.53.0"
-    }
     cloudfoundryv3 = {
       source  = "cloudfoundry/cloudfoundry"
       version = "< 2.0"
