061ee2444fec8b6a5474e7e56a211cabbb44c166
Christopher Weibel <christopher.weibel@gsa.gov>
2025-05-28 14:12:57 -0400

    Remove count

--- terraform/stacks/cf/iso.tf
+++ terraform/stacks/cf/iso.tf
@@ -11,12 +11,10 @@ resource "cloudfoundry_isolation_segment_entitlement" "platform" {
 }
 
 resource "cloudfoundry_isolation_segment" "devtools" {
-  count = var.iaas_stack_name == "development" ? 1 : 0
-  name  = "devtools"
+  name = "devtools"
 }
 
 resource "cloudfoundry_isolation_segment_entitlement" "devtools" {
-  count   = var.iaas_stack_name == "development" ? 1 : 0
   segment = cloudfoundry_isolation_segment.devtools.id
   orgs = [
     cloudfoundry_org.cloud-gov-devtools.id
