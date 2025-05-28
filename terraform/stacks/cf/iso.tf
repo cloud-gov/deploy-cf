dbf3aa25eb05a8988a8630f75e1a3fac147730a8
Christopher Weibel <christopher.weibel@gsa.gov>
2025-05-28 13:49:07 -0400

    Adding tf to associate org to iso seg

--- terraform/stacks/cf/iso.tf
+++ terraform/stacks/cf/iso.tf
@@ -9,3 +9,17 @@ resource "cloudfoundry_isolation_segment_entitlement" "platform" {
   ]
   default = false
 }
+
+resource "cloudfoundry_isolation_segment" "devtools" {
+  count = var.iaas_stack_name == "development" ? 1 : 0
+  name  = "devtools"
+}
+
+resource "cloudfoundry_isolation_segment_entitlement" "devtools" {
+  count   = var.iaas_stack_name == "development" ? 1 : 0
+  segment = cloudfoundry_isolation_segment.devtools.id
+  orgs = [
+    cloudfoundry_org.cloud-gov-devtools.id
+  ]
+  default = false
+}
