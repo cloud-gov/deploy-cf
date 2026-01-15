0d54e8e24ad20f6f59c6f71b6e0fb821025e2452
Jason <jason.gambino@gsa.gov>
2026-01-15 11:54:16 -0500

    moved it to other folder

--- terraform/stacks/cf/services.tf
+++ terraform/stacks/cf/services.tf
@@ -20,5 +20,5 @@ resource "cloudfoundry_service_instance" "rds_instance" {
   name         = "opensearch-test-db"
   space        = data.cloudfoundry_space.space.id
   service_plan = data.cloudfoundry_service_plan.rds_plan.id
-  type = "instance"
+  type = "managed"
 }
