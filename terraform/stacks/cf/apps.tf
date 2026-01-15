af870104948cff82d3fb82bfdccc3fbdfa625436
Jason <jason.gambino@gsa.gov>
2026-01-15 13:36:48 -0500

    format

--- terraform/stacks/cf/apps.tf
+++ terraform/stacks/cf/apps.tf
@@ -1,6 +1,6 @@
 module "test_cdn" {
-  count           = var.iaas_stack_name == "development" ? 0 : 1
-  source          = "../../modules/test_cdn"
-  iaas_stack_name = var.iaas_stack_name
+  count             = var.iaas_stack_name == "development" ? 0 : 1
+  source            = "../../modules/test_cdn"
+  iaas_stack_name   = var.iaas_stack_name
   organization_name = cloudfoundry_org.cloud-gov.name
 }
--- terraform/stacks/cf/services.tf
+++ terraform/stacks/cf/services.tf
@@ -11,7 +11,7 @@ data "cloudfoundry_space" "space" {
 
 # Get service plan
 data "cloudfoundry_service_plan" "rds_plan" {
-  name                 = "micro-psql"
+  name                  = "micro-psql"
   service_offering_name = "aws-rds"
 }
 
@@ -20,5 +20,5 @@ resource "cloudfoundry_service_instance" "rds_instance" {
   name         = "opensearch-test-db"
   space        = data.cloudfoundry_space.space.id
   service_plan = data.cloudfoundry_service_plan.rds_plan.id
-  type = "managed"
+  type         = "managed"
 }
