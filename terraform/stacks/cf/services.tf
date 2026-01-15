43620236c345f7bb87953773980bd6aa300a2d61
Jason <jason.gambino@gsa.gov>
2026-01-15 11:43:15 -0500

    moved it to other folder

--- terraform/stacks/cf/services.tf
+++ terraform/stacks/cf/services.tf
@@ -1,24 +1,23 @@
-resource "cloudfoundry_service_instance" "rds_instance" {
-  name             = "opensearch-test-db"
-  space        = data.cloudfoundry_space.space.id
-  service_plan = data.cloudfoundry_service_plan.rds_plan.id
-}
-
+# Get organization
 data "cloudfoundry_org" "org" {
   name = "cf_smoke_tests_org"
 }
 
+# Get space
 data "cloudfoundry_space" "space" {
   name = "cf_smoke_tests_space"
   org  = data.cloudfoundry_org.org.id
 }
 
+# Get service plan
 data "cloudfoundry_service_plan" "rds_plan" {
-  name = "micro-psql" 
-  service_offering_name = "aws-rds"  
+  name                 = "micro-psql"
+  service_offering_name = "aws-rds"
 }
 
-resource "cloudfoundry_service_key" "rds_key" {
-  name             = "opensearch-test-db-creds"
-  service_instance = cloudfoundry_service_instance.rds_instance.id
+# Create RDS service instance
+resource "cloudfoundry_service_instance" "rds_instance" {
+  name         = "opensearch-test-db"
+  space        = data.cloudfoundry_space.space.id
+  service_plan = data.cloudfoundry_service_plan.rds_plan.id
 }
