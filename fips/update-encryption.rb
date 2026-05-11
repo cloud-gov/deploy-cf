5b75ca92dc0ea0e0a28e74c107ccae600a8a636c
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-05-11 08:15:32 -0600

    output json

--- fips/update-encryption.rb
+++ fips/update-encryption.rb
@@ -1,15 +1,15 @@
-# EXISTING_ENCRYPTED_VALUE
-# EXISTING_SALT
-# CURRENT_KEY_NAME
+existing_encrypted_value = ENV['EXISTING_ENCRYPTED_VALUE']
+existing_salt = ENV['EXISTING_SALT']
+current_key_name = ENV['CURRENT_KEY_NAME']
 
-decrypted_value = Encryptor.decrypt("#{EXISTING_ENCRYPTED_VALUE}", "#{EXISTING_SALT}", iterations: 2048, label: "#{CURRENT_KEY_NAME}")
+decrypted_value = Encryptor.decrypt(existing_encrypted_value, existing_salt, iterations: 2048, label: current_key_name)
 new_salt = VCAP::CloudController::Encryptor.generate_salt
 new_encrypted_value = Encryptor.encrypt(decrypted_value, new_salt)
 
-decrypted_new_value = Encryptor.decrypt("#{new_encrypted_value}", "#{new_salt}", iterations: 2048, label: "#{CURRENT_KEY_NAME}")
+decrypted_new_value = Encryptor.decrypt(new_encrypted_value, new_salt, iterations: 2048, label: current_key_name)
 
 if decrypted_new_value != decrypted_value 
-    puts "{\"error\": \"Encypted values don't match\"}"
+    puts "UPDATE_ENCRYPTION_RESULT: {\"error\": \"Encypted values don't match\"}"
 else     
-    puts "{\"existing_encrypted_value\": \"#{EXISTING_ENCRYPTED_VALUE}\", \"existing_salt\": \"#{EXISTING_SALT}\", \"new_encrypted_value\": \"#{new_encrypted_value}\", \"new_salt\": \"#{new_salt}\"}"
+    puts "UPDATE_ENCRYPTION_RESULT: {\"existing_encrypted_value\": \"#{existing_encrypted_value}\", \"existing_salt\": \"#{existing_salt}\", \"new_encrypted_value\": \"#{new_encrypted_value}\", \"new_salt\": \"#{new_salt}\"}"
 end
