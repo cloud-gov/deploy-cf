88e95b326694519977b3fdf71e6edeb235d265d2
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-05-12 07:08:01 -0600

    debug

--- fips/migrate.sh
+++ fips/migrate.sh
@@ -35,8 +35,11 @@ get_updated_encrypted_values() {
     export EXISTING_SALT="$existing_salt"
     export CURRENT_KEY_NAME="$current_key_name"
 
+    local ruby_output
     set -x
-    cat "${this_directory}/update-encryption.rb" | /var/vcap/jobs/cloud_controller_ng/bin/console | grep "^UPDATE_ENCRYPTION_RESULT: " | sed 's/UPDATE_ENCRYPTION_RESULT: //' 
+    ruby_output=$(cat "${this_directory}/update-encryption.rb" | /var/vcap/jobs/cloud_controller_ng/bin/console)
+    ruby_output=$(echo "$ruby_output" | grep "^UPDATE_ENCRYPTION_RESULT: " | sed 's/UPDATE_ENCRYPTION_RESULT: //')
+    echo "$ruby_output"
     set +x
 }
 
