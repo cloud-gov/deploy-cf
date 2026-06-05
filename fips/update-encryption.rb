aac54f3855e6c22f839fdafe62fbef40eab17cbd
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-06-05 12:45:47 -0600

    full path

--- fips/update-encryption.rb
+++ fips/update-encryption.rb
@@ -6,10 +6,10 @@ new_salt = VCAP::CloudController::Encryptor.generate_salt
 if existing_encrypted_value == "" || existing_encrypted_value == "null"
     content="{\"existing_encrypted_value\": \"\", \"existing_salt\": \"#{existing_salt}\", \"new_encrypted_value\": \"\", \"new_salt\": \"#{new_salt}\"}"
 else 
-    decrypted_value = Encryptor.decrypt(existing_encrypted_value, existing_salt, iterations: 2048, label: current_key_name)
-    new_encrypted_value = Encryptor.encrypt(decrypted_value, new_salt)
+    decrypted_value = VCAP::CloudController::Encryptor.decrypt(existing_encrypted_value, existing_salt, iterations: 2048, label: current_key_name)
+    new_encrypted_value = VCAP::CloudController::Encryptor.encrypt(decrypted_value, new_salt)
 
-    decrypted_new_value = Encryptor.decrypt(new_encrypted_value, new_salt, iterations: 2048, label: current_key_name)
+    decrypted_new_value = VCAP::CloudController::Encryptor.decrypt(new_encrypted_value, new_salt, iterations: 2048, label: current_key_name)
 
     if decrypted_new_value != decrypted_value 
         content="{\"error\": \"Encypted values do not match\" \"existing_encrypted_value\": \"#{existing_encrypted_value}\", \"existing_salt\": \"#{existing_salt}\", \"new_encrypted_value\": \"#{new_encrypted_value}\", \"new_salt\": \"#{new_salt}\"}"
